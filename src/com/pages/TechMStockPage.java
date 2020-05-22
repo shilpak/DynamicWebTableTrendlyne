c54896d48722b03f6bf260a0adc178993433b514
shilpak <shilpa_kothari@hotmail.com>
2020-05-21 21:26:04 -0400

    changed method name and add argument

--- src/com/pages/TechMStockPage.java
+++ src/com/pages/TechMStockPage.java
@@ -2,10 +2,10 @@ package com.pages;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Properties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.testng.Assert;
 import com.base.TestBase;
 import com.util.TestUtil;
 import com.util.LocatorsPage;
@@ -16,40 +16,41 @@ public class TechMStockPage extends TestBase {
 	TestUtil testutil;
 	LocatorsPage locators;
 
-	public void validateTitle() {
-		Assert.assertEquals(driver.getTitle(), "Tech Mahindra Ltd. - Disclosures under Insider Trades & Substantial Acquisition of Shares and Takeovers");		
+	public String getTitle() {
+		return driver.getTitle();
 	}
 
-	public void printFirstColumnData() throws InterruptedException {
-		List<String> names = new ArrayList<String>();
-		driver.findElement(By.cssSelector(LocatorsPage.webTable));
-		TestUtil.scrollDownBy(driver);
+	public boolean findClientName(String clientName) throws InterruptedException {
+		try {
+			List<String> names = new ArrayList<String>();
+			driver.findElement(By.cssSelector(LocatorsPage.webTable));
+			TestUtil.scrollDownBy(driver);
+			List<WebElement> namesElements=driver.findElements(By.cssSelector(LocatorsPage.webTableFirstColumn)); 
 
-		List<WebElement> namesElements=driver.findElements(By.cssSelector(LocatorsPage.webTableFirstColumn)); 
-
-		for(WebElement namesElement : namesElements) {		    
-			names.add(namesElement.getText());
-		}
-
-		String liclass = driver.findElement(By.cssSelector(LocatorsPage.lastListElement)).getAttribute("class");
-
-		while(!liclass.contains("disabled")) {
-			WebElement nextButton = driver.findElement(By.cssSelector(LocatorsPage.lastListElement));
-			TestUtil.javaScriptClick(driver,nextButton);
-
-			namesElements=driver.findElements(By.cssSelector(LocatorsPage.webTableFirstColumn));				
 			for(WebElement namesElement : namesElements) {		    
 				names.add(namesElement.getText());
 			}
-			
-			liclass = driver.findElement(By.cssSelector(LocatorsPage.lastListElement)).getAttribute("class");
-		}
-		for(String name : names) {
-			System.out.println(name);						
-		}		
-		int totalNamesEntries = names.size();
-		System.out.println("Total count of ClientNames:" + totalNamesEntries);
+			String liclass = driver.findElement(By.cssSelector(LocatorsPage.lastListElement)).getAttribute("class");
 
+			while(!liclass.contains("disabled")) {
+				WebElement nextButton = driver.findElement(By.cssSelector(LocatorsPage.lastListElement));
+				TestUtil.javaScriptClick(driver,nextButton);
+
+				namesElements=driver.findElements(By.cssSelector(LocatorsPage.webTableFirstColumn));				
+				for(WebElement namesElement : namesElements) {		    
+					names.add(namesElement.getText());
+				}
+
+				liclass = driver.findElement(By.cssSelector(LocatorsPage.lastListElement)).getAttribute("class");
+			}
+			for(String name : names) {
+				if(name.trim().equals(clientName))
+					return true;
+			}
+			return false;
+		}catch(NoSuchElementException e){
+			return false;
+		}
 	}
 
 }
