083c287751c5fe66053009c72d31e233ca29bb2a
shilpak <shilpa_kothari@hotmail.com>
2020-05-23 21:47:07 -0400

    class to test the testdata

---
+++ src/com/util/TestData.java
@@ -0,0 +1,33 @@
+package com.util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+import com.base.TestBase;
+
+public class TestData extends TestBase {
+	
+	public static WebDriver driver;
+	public static Properties properties;
+
+	public TestData() {
+		
+		properties = new Properties();
+		try {
+			FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"/src/com/config/testData.properties");
+			properties.load(file);
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
+
